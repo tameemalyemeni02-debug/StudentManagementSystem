9d123e6f4b8865f976e23debd96a6329dd22fc90
tameemalyemeni02-debug <tameemalyemeni02@gmail.com>
2025-09-04 05:07:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program<T>
+    {
+        private string name;
+        private string description;
+        private T salary;
+
+        public Program(string name,string description, T salary)
+        {
+            this.name = name;
+            this.description = description;
+            this.salary = salary;
+        }
+        public void printDetails()
+        {
+            Console.WriteLine($"name: {name}");
+            Console.WriteLine($"description: {description}");
+            Console.WriteLine($"salary: {salary}");
+        }
+        static void Main(string[] args)
+        {
+            Program<double> b1= new Program<double>("noor","it",5000.0);
+            b1.printDetails();
+        }
+    }
+}
